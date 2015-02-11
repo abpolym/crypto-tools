f7269f4805dfcef76833dc26e64826682b347c49
YASME-Tim <hu78sapy@stud.cs.fau.de>
2015-02-11 12:25:00 +0100

    :muscle: Add decryption for poem code

--- poemcode/poemcode.py
+++ poemcode/poemcode.py
@@ -14,6 +14,7 @@ def encrypt(code, poem, msg):
 	plen = len(pwords)
 
 	if plen > len(abc): sys.exit(3)
+
 	pcode = [None] * plen
 	count = 0
 	while(count<plen):
@@ -47,12 +48,42 @@ def encrypt(code, poem, msg):
 		cip+=' '
 	return cip
 
-def decrypt():
-	print 'Decrypt'
+def decrypt(poem, cip):
+	msg = ''
+	twords = []
+	for line in open(poem,'r'):
+		for w in line.split(): twords.append(w.lower())
+
+	cwords = []
+	for line in open(cip, 'r'):
+		for w in line.split(): cwords.append(w.lower())
+
+	code = []
+	for i in cwords.pop(0):
+		code.append(abc.index(i))
+	
+	pwords = ''
+	for c in code: pwords += twords[c].lower()
+	plen = len(pwords)
+
+	pcode = [None] * plen
+	count = 0
+	while(count<plen):
+		for al in abc:
+			for pc, pl in enumerate(pwords):
+				if al!=pl: continue
+				pcode[count]=cwords[pc]
+				count+=1
+
+	wlen = len(pcode[0])
+	for c in range(0, wlen):
+		for word in pcode:
+				msg+=word[c]
+	return msg
 
 # first argument = poem
 # second argument = ciphertxt or msg
 if len(sys.argv) != 3: sys.exit(2)
 
-code = [0, 5, 13, 16, 19]
-print encrypt(code, sys.argv[1], sys.argv[2])
+#print encrypt([0, 5, 13, 16, 19], sys.argv[1], sys.argv[2])
+print decrypt(sys.argv[1], sys.argv[2])
