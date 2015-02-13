8f4f44e823c9f93506b3ec2110fcf644cdc20b99
YASME-Tim <hu78sapy@stud.cs.fau.de>
2015-02-13 18:32:19 +0100

    :muscle: Add uuencode scheme

--- find-coding-scheme/find_coding_scheme.py
+++ find-coding-scheme/find_coding_scheme.py
@@ -5,5 +5,8 @@ import re
 if len(sys.argv) != 2: sys.exit(2)
 estr=sys.argv[1]
 
-b64rex = re.compile('^[A-Za-z0-9+/]+$')
+b64rex = re.compile('^[A-Za-z0-9+/]+[=]{0,2}$')
 if(b64rex.match(estr)): print 'base64'
+
+uurex = re.compile('^(begin.*\n)?[\x20-\x60\n]+(end[\n]?)?$')
+if(uurex.match(estr)): print 'uuencode'
